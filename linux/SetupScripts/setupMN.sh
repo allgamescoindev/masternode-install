6aa7e0f69d00c997f85b55f54a4d6ae8190c0aac
allgamecoin-dev <sun_stone@outlook.com>
2018-09-06 04:14:18 +0000

    fix bug

--- linux/SetupScripts/setupMN.sh
+++ linux/SetupScripts/setupMN.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
+output() {
+    printf "\E[0;33;40m"
+    echo $1
+    printf "\E[0m"
+}
 
-#Update and install some dependencies
+#Input
+output "Make sure you have mortgaged 1000XAGC in your local wallet."
+output ""
+read -e -p "Please enter your masternode key and press ENTER: " masternodekey
+read -e -p "Install UFW and configure ports? [Y/n] : " UFW
+output ""
 
-apt-get update;
-apt-get upgrade -y;
-apt-get install -y zip unzip curl git wget;
-RED='\033[0;31m';
-GREEN='\033[0;32m';
-NC='\033[0m'; \
+output "Update and install some dependencies"
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt-get install -y zip unzip curl git wget;
 
-
-# Check if firewall installed, if so open required ports.
-echo -e "$RED=== Checking if firewall is installed/active ===$NC";
-ufw status | grep -qw active;
-if (( $? == 0 ));then
-    echo "$RED=== Firewall found, opening required ports ===$NC";
-    ufw allow 22;
-    ufw allow 7207;
-    ufw allow 7208;
-    ufw enable;
+output "Install UFW and configure ports"
+sudo apt-get install ufw
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow 22;
+sudo ufw allow 7207;
+sudo ufw allow 7208;
+sudo ufw --force enable    
 fi
 
-echo -e "$RED=== Checking if total system memory is below 2 GB, if so create swap space ===$NC";
+output "Checking if total system memory is below 2 GB, if so create swap space"
 #Check if swap need to be enabled.
 totalk=$(awk '/^MemTotal:/{print $2}' /proc/meminfo) ;
-if (( $totalk <  1999999 )) ; then
-    echo -e "$RED=== Total system memory is below 2 GB, attempting to create swap space ===$NC";
+if (( $totalk -lt 1999999 )) ; then
+    output "Total system memory is below 2 GB, attempting to create swap space";
     cd;
-    dd if=/dev/zero of=swapfile bs=1M count=3000;
-    mkswap swapfile;
-    chmod 600 swapfile;
-    swapon swapfile;
+    sudo dd if=/dev/zero of=swapfile bs=1M count=3000;
+    sudo mkswap swapfile;
+    #sudo chmod 600 swapfile;
+    sudo swapon swapfile;
     echo "/swapfile none swap sw 0 0" >> /etc/fstab;
 fi
 
-echo -e "$RED=== Downloading AllGamesCoin wallet/daemon ===$NC";
+output "Downloading AllGamesCoin wallet/daemon";
 cd ~;
 SYSTEM=$(uname -m);
 if [ "$SYSTEM" = "x86_64" ]; then
@@ -45,22 +52,19 @@ else
     unzip -o ~/allgamescoin-v0.2.0-linux-32bits.zip -d ~/wallet/;
 fi
 
-echo -e "$RED=== Installing AllGamesCoin wallet/daemon ===$NC";
+output "Installing AllGamesCoin wallet/daemon";
 install -m 0755 -o root -g root -t /usr/local/bin ~/wallet/*;
 mkdir ~/.allgamescoincore;
 RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1);
 RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1);
 NODEIP=$(curl -s4 api.ipify.org);
 
-
-echo -e "$RED=== Setting up config ===$NC";
-read -p "Please enter your masternode key and press ENTER: " masternodekey
-
+output "Setting up config";
 echo "rpcuser=$RPCUSER" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "rpcpassword=$RPCPASSWORD" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "rpcallowip=127.0.0.1" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "rpcport=7207" >> ~/.allgamescoincore/allgamescoin.conf;
-#echo "bind=$NODEIP" >> ~/.allgamescoincore/allgamescoin.conf;
+echo "bind=$NODEIP" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "server=1" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "listen=1" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "daemon=1" >> ~/.allgamescoincore/allgamescoin.conf;
@@ -71,28 +75,23 @@ echo "externalip=$NODEIP:7208" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "masternode=1" >> ~/.allgamescoincore/allgamescoin.conf;
 echo "masternodeprivkey=$masternodekey" >> ~/.allgamescoincore/allgamescoin.conf;
 
-echo "allgamescoind --daemon -datadir=/root/.allgamescoincore" >> /etc/rc.local;
+sudo sed -i '13i\allgamescoind --daemon -datadir=$HOME/.allgamescoincore'  /etc/rc.local;
 
-echo -e "$RED=== Installing Sentinel ===$NC";
+output "Run allgamescoind";
+allgamescoind --daemon -datadir=$HOME/.allgamescoincore
+
+output "Installing Sentinel";
 apt-get install python3-pip -y;
 pip3 install virtualenv;
 cd ~;
 git clone https://github.com/allgamescoindev/sentinel.git && cd sentinel;
 virtualenv ./venv;
 ./venv/bin/pip install -r requirements.txt;
+echo "* * * * *    root    cd /root/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> /etc/crontab
 
-
-read -p "Attempt to add job for sentinel to crontab? (if this fails, add it manually please)  [Yy] ";
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    crontab -l > mycron;
-    echo "* * * * * cd /root/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> mycron;
-    crontab mycron;
-    rm mycron;
-fi
-
-echo -e "$GREEN=== Install script finished! ===$NC";
-echo -e "$GREEN=== Your Node ip: $NODEIP:7208 ===$NC";
-echo -e "$GREEN=== Start your node by running command: 'allgamescoind --daemon -datadir=/root/.allgamescoincore' ===$NC";
-echo -e "$GREEN=== Check node status with command: 'allgamescoin-cli masternode status' ===$NC";
-echo -e "$GREEN=== Check blockchain sync with command: 'allgamescoin-cli getblockcount' ===$NC";
+output "=== Install script finished! ";
+output "=== Your Node ip: $NODEIP:7208 | rpcuser=$RPCUSER | rpcpassword=$RPCPASSWORD";
+output "=== Start your node by running command: allgamescoind --daemon -datadir=$HOME/.allgamescoincore ";
+output "=== Check blockchain sync with command: allgamescoin-cli getblockcount ";
+output "=== Check node status with command: allgamescoin-cli masternode status ";
+output "=== Local wallet masternode.conf : mn1 $NODEIP:7208 $masternodekey tx_id(look for it in history) digit(0 or 1) ";
